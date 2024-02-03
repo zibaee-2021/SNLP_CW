27abd9aad7e438ad3198a1f137acbfb16ce960d0
Yufei-Gu-451 <yufei.gu.job@gmail.com>
2024-02-03 18:53:35 +0000

    Evaluation with json dictionary parser

--- QALM_mcq.py
+++ QALM_mcq.py
@@ -11,8 +11,12 @@ from langchain_core.runnables import RunnableLambda, RunnableParallel
 
 from langchain_openai import ChatOpenAI
 
+from tqdm import tqdm
+
 import torch
 import pandas as pd
+import json
+import ast
 import re
 
 def load_llama(model_path):
@@ -31,15 +35,18 @@ def load_llama(model_path):
 
     return llm
 
-def find_json(input_string):
+def read_dict(input_string):
     # Define a regular expression to capture content between curly braces
-    pattern = re.compile(r'```\{ \}```')
+    pattern1 = re.compile(r'```json\n(.*?)\n```', re.DOTALL)
+    pattern2 = re.compile(r'```\n(.*?)\n```', re.DOTALL)
 
     # Find the first match in the input string
-    match = pattern.search(input_string)
-
-    # Return the captured content or None if no match is found
-    return match.group(1) if match else None
+    if pattern1.search(input_string):
+        return 1, pattern1.search(input_string).group(1)
+    elif pattern2.search(input_string):
+        return 2, pattern2.search(input_string).group(1)
+    else:
+        return 0, None
 
 if __name__ == '__main__':
     # Load the llama model
@@ -48,7 +55,7 @@ if __name__ == '__main__':
     # model = ChatOpenAI(temperature=0)
 
     # Assuming 'data.json' is your JSON file
-    json_file_path = 'dataset/QALM/train/mcq/bioasq_mcq_train.jsonl'
+    json_file_path = 'dataset/QALM/test/mcq/bioasq_mcq_test.jsonl'
 
     # Open the file and load its content into a dictionary
     df = pd.read_json(json_file_path, lines=True)
@@ -59,7 +66,7 @@ if __name__ == '__main__':
 
     # Set the schema for structured output
     response_schemas = [
-        ResponseSchema(name="option", description="The right option that answers the question."),
+        ResponseSchema(name="answer", description="The correct option answer."),
         ResponseSchema(name="explanation", description="The explanation to the right option.")
     ]
 
@@ -69,35 +76,65 @@ if __name__ == '__main__':
 
     prompt_template = PromptTemplate(input_variables=['text', 'prompt', 'question_type'],
                                      partial_variables={"format_instructions": format_instructions},
-                                     template='{text} {prompt} about {question_type}. \n{format_instructions}.')
+                                     template='{prompt} about {question_type}: {text} '
+                                              'Provide the answer as one capital letter. {format_instructions}.')
 
     retry_parser = RetryOutputParser.from_llm(parser=output_parser, llm=model)
 
-    for index, row in df.iterrows():
+    n_true, n_false, n_invalid = 0, 0, 0
+
+    # Go through the questions in the dataframe
+    for index, row in tqdm(df.iterrows(), total=df.shape[0], desc="Processing Rows"):
         text = row['text']
         prompt = row['prompt']
         question_type = row['question_type']
 
         print('------------------------------------------------------------')
 
-        print(f'{text} {prompt} under the topic of {question_type}.')
-
-        chain = prompt_template | model | output_parser
-
-        main_chain = RunnableParallel(
-            completion=chain, prompt_value=prompt_template
-        ) | RunnableLambda(lambda x: retry_parser.parse_with_prompt(**x))
-
-        output = main_chain.invoke({'text': text, 'prompt': prompt, 'question_type': question_type})
-
-        #final_prompt = prompt_template.format(text=text,
-        #                                      prompt=prompt,
-        #                                      question_type=question_type,
-        #                                      format_instructions=format_instructions)
-        #output = model(final_prompt)
-
-        print('\nLLM: ', output)
+        print(f'{prompt} about {question_type}: {text}')
 
         print('\nGolden answer: ', row['answer'])
 
+        final_prompt = prompt_template.format(text=text,
+                                              prompt=prompt,
+                                              question_type=question_type,
+                                              format_instructions=format_instructions)
+
+        output = model(final_prompt)
+
+        i, captured_dict = read_dict(output)
+        print(i, captured_dict)
+
+        if captured_dict:
+            '''
+            if i == 1:
+                try:
+                    dict = json.loads(captured_dict)
+                except json.JSONDecodeError as e:
+                    print(f"\nError decoding Dict with json: {captured_dict}")
+                    n_invalid += 1
+                    continue
+            '''
+            if i == 1 or i == 2:
+                try:
+                    dict = ast.literal_eval(captured_dict)
+                except (ValueError, SyntaxError) as e:
+                    print(f"\nError decoding Dict with text: {captured_dict}")
+                    n_invalid += 1
+                    continue
+            else:
+                raise NotImplementedError
+
+            print(f"\nCorrectly captured json dictionary: {dict}")
+
+            if dict['answer'] == row['answer']:
+                n_true += 1
+            else:
+                n_false += 1
+        else:
+            print(f"\nNot captured json with text: {output}")
+            n_invalid += 1
+
         print('------------------------------------------------------------')
+
+        print(n_true, n_false, n_invalid)
