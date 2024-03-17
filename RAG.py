4739d791b6fdb620a5baadf2eb2eff21e9fd0cd4
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2024-03-17 18:39:12 +0000

    For Colab: 1. Add a bunch of print statements. 2. Add tqdm and numpy imports. 3. Set openai key to my one. 4. Load to false. 5. Uncomment experiment at the end. The code currently fails at FAISS.from_documents with: ```index = faiss.IndexFlatL2(len(embeddings[0])) IndexError: list index out of range```

--- RAG.py
+++ RAG.py
@@ -3,52 +3,62 @@ from langchain.text_splitter import TokenTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-
+from tqdm import tqdm 
+import numpy as np
 import json
 import os
+
+selected_embedding_model = 'OpenAI'
+
+
+# from google.colab import userdata
+# userdata.get('shahin_openai_key')
 
 from Datasets import BioASQ
 
 # os.environ["OPENAI_API_KEY"] = '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'
-# os.environ["OPENAI_API_KEY"] = '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'
-os.environ["OPENAI_API_KEY"] = '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'
+openaikey = os.environ["OPENAI_API_KEY"] = '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'
+# os.environ["OPENAI_API_KEY"] = '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'
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 def load_medrag_pubmed_json(json_path_list):
+    print(f'Called load_medrag_pubmed_json()')
     docs = []
-
-    for file_path in json_path_list:
-        # Load JSON file
+    for file_path in tqdm(json_path_list):
+        # Load JSON file 
+        # (this code here by Yufei is for opening a jsonl file, 
+        # but the downloaded files from MedRAG are not jsonl .. ?)
         with open(file_path, 'r', encoding='UTF-8') as file:
+            line_count = sum(1 for line in file)
+            print(f'There are {line_count} JSON objects in {file_path}.')
             for line in file:
                 data = json.loads(line)
-
                 metadata = {"id": data.get("id"),
                             "title": data.get("title")}
-
                 docs.append(Document(page_content=data.get('content'), metadata=metadata))
-
     return docs
 
 
 def get_embeddings(embedding_model):
+    print('Called get_embeddings()')
     if embedding_model == 'e5':
         embeddings = HuggingFaceEmbeddings(
             model_name="intfloat/e5-large-unsupervised",
             model_kwargs={'device': 'cpu'},
             encode_kwargs={'normalize_embeddings': False}
         )
-
-    elif embedding_model == 'OpenAI':
+    elif embedding_model == selected_embedding_model:
+        print('\nSHOULD ENTER here in elif embedding_model')
         embeddings = OpenAIEmbeddings()
+        print(f'embeddings={embeddings}')
     else:
         raise NotImplementedError
-
     return embeddings
 
 
 def save_faiss_database(documents, file_path_list, embedding_model):
+    print('Called save_faiss_database')
     # Load Document from File Path
     # data = load_docs(documents, document_file_path)
     docs = load_medrag_pubmed_json(file_path_list)
@@ -59,6 +69,7 @@ def save_faiss_database(documents, file_path_list, embedding_model):
 
     # Get the Embeddings Model
     embeddings = get_embeddings(embedding_model)
+    print(f'****type(embeddings): {type(embeddings)}****')
 
     # Setup database
     database = FAISS.from_documents(chunks, embeddings)
@@ -69,29 +80,28 @@ def save_faiss_database(documents, file_path_list, embedding_model):
 
 def load_faiss_database(documents, embedding_model):
     embeddings = get_embeddings(embedding_model)
-
     # Load database from file
     database = FAISS.load_local(f"FAISS/faiss_index_{documents}_{embedding_model}", embeddings)
-
     return database
 
 
 if __name__ == '__main__':
     file_path_list = []
-    for i in range(1, 11):
+    for i in tqdm(range(1, 11)):
+        print(f'pubmed23n000{i}.jsonl')
         file_path_list.append('refs/pubmed/chunk/pubmed23n{:04d}.jsonl'.format(i))
 
-    load = True
+    # load = True
+    load = False
 
     if load:
-        retrieval_model = load_faiss_database(documents='BioASQ_11B_test',
+        retrieval_model = load_faiss_database(documents='index_BioASQ_11B_test',
                                               embedding_model='OpenAI')
     else:
         retrieval_model = save_faiss_database(documents='BioASQ_11B_test',
                                               file_path_list=file_path_list,
                                               embedding_model='OpenAI')
 
-    '''
     # ------------------------------------------------------------------------------------------------------------------
 
     data = BioASQ(['dataset/Task11BGoldenEnriched/11B1_golden.json',
@@ -103,15 +113,15 @@ if __name__ == '__main__':
 
     matching_ratio = []
 
-    for question in yesno_questions:
+    for question in tqdm(yesno_questions):
+        print('Going through BioASQ yesno_questions')
         retrieved_docs = retrieval_model.similarity_search(question.question_body, k=len(question.docs))
-
         print(question.question_body)
-
         print(question.docs)
-
         count = 0
+
         for doc in retrieved_docs:
+            print('Going through retrieved docs')
             print(doc.metadata['url'])
             if doc.metadata['url'] in question.docs:
                 count += 1
@@ -121,4 +131,4 @@ if __name__ == '__main__':
         matching_ratio.append(count / len(retrieved_docs))
 
     print("\nMatching Ratio (Retrieved URL's Percentage in Golden Refs:", np.mean(matching_ratio))
-    '''
+    
